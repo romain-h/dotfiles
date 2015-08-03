9af1aef8126e70f9a2da86fb6ef2e59b7897208c
Romain Hardy <romain.hardy17@gmail.com>
2015-08-03 20:38:32 +0100

    Add defaults mac + script to install apps

---
+++ osx/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Install Jumpcut
+curl -L http://downloads.sourceforge.net/jumpcut/Jumpcut_0.63.tgz > /var/tmp/jumpcut.tgz && tar -xf /var/tmp/jumpcut.tgz && mv /var/tmp/Jumpcut.app /Applications/
+
+# TODO add to default login
