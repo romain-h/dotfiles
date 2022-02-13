18ee24095c57a4f37ed1eff06e68ea26bb86f985
Romain Hardy <romainhardy@monzo.com>
2022-02-13 13:48:06 +0100

    Remove unused tools

--- bin/clipper.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-# If reattach-to-user-namespace is not available, just run the command.
-if [ -n "$(command -v reattach-to-user-namespace)" ]; then
-  reattach-to-user-namespace pbcopy
-else
-  # Clipper port
-  nc localhost 8377
-fi
