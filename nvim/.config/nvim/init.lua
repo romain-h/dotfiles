45c4aa5c4d8f5f5a6ae2f6b76f6b4f0da18b745b
Romain Hardy <romain.hardy17@gmail.com>
2026-05-06 11:52:27 +0200

    nvim: try snack

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -243,6 +243,170 @@ require("lazy").setup({
 	"tpope/vim-vinegar", -- Enhances netrw
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			explorer = { enabled = false },
+			indent = { enabled = false },
+			input = { enabled = false },
+			notifier = {
+				enabled = true,
+				timeout = 3000,
+			},
+			picker = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = false },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = false },
+			words = { enabled = false },
+			styles = {
+				notification = {
+					-- wo = { wrap = true } -- Wrap notifications
+				}
+			}
+		},
+		keys = {
+			-- Top Pickers & Explorer
+			{ "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+			{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+			{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+			{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+			{ "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+			{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+			-- find
+			{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+			{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+			{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+			{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+			{ "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+			{ "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+			-- git
+			{ "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+			{ "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+			{ "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+			{ "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+			{ "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+			{ "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+			{ "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+			-- gh
+			{ "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+			{ "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+			{ "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+			{ "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+			-- Grep
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+			{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+			{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+			-- search
+			{ '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+			{ '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
+			{ "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+			{ "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+			{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+			{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+			{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+			{ "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+			{ "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+			{ "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+			{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+			{ "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+			{ "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+			{ "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+			{ "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+			{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+			{ "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+			{ "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+			{ "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+			-- LSP
+			{ "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+			{ "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+			{ "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+			{ "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+			{ "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+			{ "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
+			{ "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
+			{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+			{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+			-- Other
+			{ "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+			{ "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+			{ "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+			{ "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+			{ "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+			{ "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+			{ "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+			{ "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+			{ "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+			{ "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+			{ "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+			{ "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+			{ "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+			{ "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+			{
+				"<leader>N",
+				desc = "Neovim News",
+				function()
+					Snacks.win({
+						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+						width = 0.6,
+						height = 0.6,
+						wo = {
+							spell = false,
+							wrap = false,
+							signcolumn = "yes",
+							statuscolumn = " ",
+							conceallevel = 3,
+						},
+					})
+				end,
+			}
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+
+					-- Override print to use snacks for `:=` command
+					if vim.fn.has("nvim-0.11") == 1 then
+						vim._print = function(_, ...)
+							dd(...)
+						end
+					else
+						vim.print = _G.dd
+					end
+
+					-- Create some toggle mappings
+					Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+					Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+					Snacks.toggle.diagnostics():map("<leader>ud")
+					Snacks.toggle.line_number():map("<leader>ul")
+					Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+					Snacks.toggle.treesitter():map("<leader>uT")
+					Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+					Snacks.toggle.inlay_hints():map("<leader>uh")
+					Snacks.toggle.indent():map("<leader>ug")
+					Snacks.toggle.dim():map("<leader>uD")
+				end,
+			})
+		end,
+	},
+
+
 	-- Git integration
 	{ "tpope/vim-fugitive", dependencies = { "tpope/vim-rhubarb" } },
 
@@ -564,88 +728,14 @@ require("lazy").setup({
 			-- Layer Monzo-specific LSP overrides (noop if module doesn't exist)
 			local ok, monzo_lsp = pcall(require, "monzo.lsp")
 			if ok then
+				local overridden = {}
 				for name, overrides in pairs(monzo_lsp.server_overrides(servers)) do
 					servers[name] = vim.tbl_deep_extend("force", servers[name] or {}, overrides)
+					table.insert(overridden, name)
 				end
-			end
-
-			if is_light_profile then
-				-- Tweak gopls
-				servers.gopls = {
-					on_init = function(client)
-						local path = client.workspace_folders[1].name
-
-						if string.find(path, "monzo/wearedev") then
-							client.config.settings.gopls["local"] = "github.com/monzo/wearedev"
-
-							-- Wearedev-specific optimizations for large monorepo
-							client.config.settings.gopls["expandWorkspaceToModule"] = false
-
-							-- Extensive directory filtering for wearedev (no wildcards, specific patterns)
-							client.config.settings.gopls["directoryFilters"] = {
-								"-vendor",
-								"-node_modules",
-								"-static-check-rules",
-								"-tools",
-								"-bin",
-								"-docs",
-								"-examples",
-								"-infrastructure",
-								"-catalog",
-								"-aws-acm-pca-issuer",
-								"-base-manifests",
-								"-ecr-login-wrapper",
-								"-enclave-parent-runtime",
-								"-feast-features",
-								"-graphql-apollo-server-v2",
-								"-ipsec-exporter",
-								"-kafka-readiness",
-								"-keyspaces-check-rules",
-								-- Explicitly list some common service prefixes
-								"-cron.account-properties-export",
-								"-cron.ach-file-generator",
-								"-cron.additional-risk-assessment-reminders",
-								"-edge-proxy-external",
-								"-edge-proxy-internal",
-								"-lambda.dc-out-of-band-ssh-certs",
-								"-k8s-auditing-sidecar",
-								"-k8s-docker-journald-fix",
-								"-k8s-ec2-zone-writer",
-								"-interconnect-v2",
-								"-interconnect",
-								"-envoy-control-plane",
-								"-envoy-preflight",
-								"-envoy"
-							}
-
-							-- Disable expensive features for performance
-							client.config.settings.gopls["codelenses"] = {
-								generate = false,
-								gc_details = false,
-								test = false,
-								tidy = false,
-								regenerate_cgo = false,
-								upgrade_dependency = false,
-								run_govulncheck = false,
-								vendor = false
-							}
-
-						end
-
-						client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-						return true
-					end,
-					-- cmd = { "gopls", "-remote=auto", "serve", "-rpc.trace", "--debug=localhost:6060" },
-					filetypes = { "go", "gomod" },
-
-					-- Ignore typical project roots which cause gopls to ingest large monorepos.
-					--root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-					-- root_dir = util.root_pattern("main.go", "README.md", "LICENSE"),
-
-					-- settings = {
-						-- gopls = {},
-					-- },
-				}
+				if #overridden > 0 then
+					vim.notify("monzo/lsp: overriding " .. table.concat(overridden, ", "), vim.log.levels.INFO)
+				end
 			end
 
 			--
